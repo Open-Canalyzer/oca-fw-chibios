03b6e051b7a90a81d5d7a45bea932a2b8fcc20de
Jerry Jacobs <jerry.jacobs@xor-gate.org>
2016-07-02 15:36:28 +0200

    travis: Handle the cache toolchain correctly

--- .travis.sh
+++ .travis.sh
@@ -5,14 +5,23 @@ GCC_ARM_EMBEDDED_TOOLCHAIN="gcc-arm-none-eabi-5_4-2016q2"
 GCC_ARM_EMBEDDED_TOOLCHAIN_TAR="${GCC_ARM_EMBEDDED_TOOLCHAIN}-20160622-linux.tar.bz2"
 GCC_ARM_EMBEDDED_TOOLCHAIN_URL="https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q2-update/+download/${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR}"
 
-if [ ! -f "${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR}" ]; then
-	echo "=== Downloading ${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR}"
-	wget ${GCC_ARM_EMBEDDED_TOOLCHAIN_URL}
+if [ ! -d "${GCC_ARM_EMBEDDED_TOOLCHAIN}" ]; then
+	if [ ! -f "${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR}" ]; then
+		echo "=== Downloading ${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR}"
+		wget ${GCC_ARM_EMBEDDED_TOOLCHAIN_URL}
+	else
+		echo "=== Toolchain ${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR} already downloaded"
+	fi
+
+	echo "=== Extracting ${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR}"
+	tar -xf ${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR}
 fi
 
-echo "=== Extracting ${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR}"
-tar -xf ${GCC_ARM_EMBEDDED_TOOLCHAIN_TAR}
-
 export PATH="${PWD}/${GCC_ARM_EMBEDDED_TOOLCHAIN}/bin:${PATH}"
+
+echo "=== Toolchain info ==="
 arm-none-eabi-gcc --version
+echo "==="
+
+
 make
